18c3e599b42a8e20aacdad28783e76a375ac0442
takeshi8989 <htakeshi0614@gmail.com>
2025-12-10 21:29:47 -0800

    import path

--- yonokuni/selfplay/__init__.py
+++ yonokuni/selfplay/__init__.py
@@ -1,11 +1,18 @@
 """Self-play and replay buffer utilities."""
 
 from .replay_buffer import ReplayBuffer, ReplaySample
-from .self_play import MCTSPolicy, RandomPolicy, SelfPlayManager, make_mcts_policy_from_model
+from .self_play import (
+    EarlyTerminationConfig,
+    MCTSPolicy,
+    RandomPolicy,
+    SelfPlayManager,
+    make_mcts_policy_from_model,
+)
 
 __all__ = [
     "ReplayBuffer",
     "ReplaySample",
+    "EarlyTerminationConfig",
     "MCTSPolicy",
     "make_mcts_policy_from_model",
     "RandomPolicy",
